1180d418fa9178034482c5b90e26dd523e932a56
Albert Folch <albertfolch@redeemeum.com>
2024-04-17 16:11:12 +0200

    fix: global style in purchase overview widget

--- src/components/widgets/styles.tsx
+++ src/components/widgets/styles.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 export const Pre = styled.pre`
   all: unset;
@@ -15,4 +15,77 @@ export const Widget = styled.div`
   display: flex;
   gap: 1rem;
   flex-direction: column;
+`;
+
+export const GlobalStyle = createGlobalStyle`
+  html, body, #root {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-rendering: optimizeLegibility;
+
+    font-family: "Plus Jakarta Sans", sans-serif;
+
+    margin: 0;
+    padding: 0;
+    font-style: normal;
+
+    height: unset;
+  }
+  #root{
+    display: flex;
+  }
+
+
+  a,
+  button,
+  input,
+  select,
+  textarea {
+    text-decoration: none;
+    &:focus,
+    &:hover {
+      outline: none;
+    }
+    cursor: pointer;
+  }
+
+  select {
+    -webkit-appearance: none;
+  }
+
+  input {
+    user-select: text;
+  }
+
+  * > small {
+    font-size: 65%;
+    margin: 0 0.5rem;
+    opacity: 0.75;
+    white-space: pre;
+  }
+
+  h1 {
+    font-size: 3.5rem;
+    font-weight: 600;
+    line-height: 1.2;
+  }
+  h2 {
+    font-size: 2rem;
+    font-weight: 600;
+    line-height: 1.2;
+  }
+  h3 {
+    font-size: 1.5rem;
+    font-weight: 600;
+    line-height: 1.5;
+  }
+  h4, h5, h6 {
+    font-size: 1.25rem;
+    line-height: 1.5;
+    margin: 0 0 1rem 0;
+  }
+  a, p, span, div {
+    font-size: 1rem;
+    line-height: 1.5;
+  }
 `;
